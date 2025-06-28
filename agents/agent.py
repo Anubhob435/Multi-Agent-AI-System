568757fdc754716115a5515184205f09d5689bc9
Anubhob <anubhob493@gmail.com>
2025-06-29 02:05:07 +0530

    adk agents added

--- agents/agent.py
+++ agents/agent.py
@@ -1,67 +1,24 @@
 import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
+from google.adk.agents import LlmAgent
+from google.adk.tools.agent_tool import AgentTool
+from .sub_agents.websearch import websearch_agent
+from .sub_agents.weather import weather_agent
 
-def get_weather(city: str) -> dict:
-    """Retrieves the current weather report for a specified city.
+MODEL = "gemini-2.5-flash"  # Or your preferred Gemini model
 
-    Args:
-        city (str): The name of the city for which to retrieve the weather report.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    if city.lower() == "new york":
-        return {
-            "status": "success",
-            "report": (
-                "The weather in New York is sunny with a temperature of 25 degrees"
-                " Celsius (77 degrees Fahrenheit)."
-            ),
-        }
-    else:
-        return {
-            "status": "error",
-            "error_message": f"Weather information for '{city}' is not available.",
-        }
-
-
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the current time.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-
-    if city.lower() == "new york":
-        tz_identifier = "America/New_York"
-    else:
-        return {
-            "status": "error",
-            "error_message": (
-                f"Sorry, I don't have timezone information for {city}."
-            ),
-        }
-
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    report = (
-        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
-    )
-    return {"status": "success", "report": report}
 
 
 root_agent = Agent(
     name="weather_time_agent",
     model="gemini-2.0-flash",
     description=(
-        "Agent to answer questions about the time and weather in a city."
+        "Agent to search anything on web and get weather report."
     ),
     instruction=(
         "You are a helpful agent who can answer user questions about the time and weather in a city."
     ),
-    tools=[get_weather, get_current_time],
+    tools=[ AgentTool(agent=websearch_agent),
+           AgentTool(agent=weather_agent)],
 )
